8b1ff0e07f2957e55f43c56e9182591c96a527d6
tsungyu927 <tsungyu927@gmail.com>
2024-05-05 18:35:18 +0800

    style: update rwd bug of the Wrapper component

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -2,7 +2,7 @@ const Wrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="relative w-full h-fit flex">
       <div className="relative hidden grow border-r sm:flex " />
-      <div className="relative min-w-full sm:min-w-[768px] w-2/3">
+      <div className="relative min-w-full lg:min-w-[768px] w-2/3">
         {children}
       </div>
       <div className="hidden sm:flex grow border-l" />
